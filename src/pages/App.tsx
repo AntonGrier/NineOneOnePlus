8b0ce739a88f9dcfa514d98206975dcc94aa81c9
Liang Liu <stuffbyliang@gmail.com>
2021-09-18 10:37:14 -0700

    Add main theme object

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,15 @@
-import { Router, Link } from '@reach/router'
+import { Router } from '@reach/router'
+import { theme } from './theme'
 import { DashboardPage } from './dashboardPage'
 import { UserPage } from './userPage'
+import { ThemeProvider } from '@mui/material/styles'
 export const App = () => {
   return (
-    <Router id='router'>
-      <UserPage path='/' />
-      <DashboardPage path='dashboard' />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router id='router'>
+        <UserPage path='/' />
+        <DashboardPage path='/dashboard' />
+      </Router>
+    </ThemeProvider>
   )
 }
