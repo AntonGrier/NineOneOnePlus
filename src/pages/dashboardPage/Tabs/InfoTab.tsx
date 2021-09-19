5e13599337f70adc3d073c4f365f75e48ebc68ea
Andrew Xie <andrewxieax1@gmail.com>
2021-09-18 22:39:18 -0700

    userinfo get request

--- src/pages/dashboardPage/Tabs/InfoTab.tsx
+++ src/pages/dashboardPage/Tabs/InfoTab.tsx
@@ -1,5 +1,18 @@
 import { FunctionComponent } from 'react'
+import axios from 'axios'
+
+const URI =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000'
+    : 'https://htn2021be.herokuapp.com/'
+
+export const getUserInfo = async () => {
+  const result = await axios.get(`${URI}/userinfo`)
+  console.log(result)
+  return result
+}
 
 export const InfoTab: FunctionComponent = () => {
-  return <>info</>
+  const usr = getUserInfo()
+  return <>usr</>
 }
